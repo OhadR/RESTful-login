55190915fcac9cb2c835ef7b9c618ef9502f2b17
OhadR <ohadr.developer@gmail.com>
2017-06-18 00:13:52 +0300

    fix spring-db-config (use the correct schema)

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,7 +1,7 @@
 create table `restful-login`.sec_token (
   ID BIGINT(20) NOT NULL AUTO_INCREMENT,
-  token VARCHAR(256),
-  user_name VARCHAR(256),
+  access_token VARCHAR(256),
+  username VARCHAR(256),
   refresh_token VARCHAR(256),
   expiration_date DATETIME,
   PRIMARY KEY (`ID`)
